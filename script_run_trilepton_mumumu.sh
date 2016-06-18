dd488da0b14491c5338368d7ad5f41b40b8b4026
Jae Sung Kim <jae.sung.kim@cern.ch>
2016-06-18 13:35:57 +0900

    script code

---
+++ script_run_trilepton_mumumu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+root -l -b -q "run_trilepton_mumumu.cc(0)"
+root -l -b -q "run_trilepton_mumumu.cc(1)"
+root -l -b -q "run_trilepton_mumumu.cc(2)"
+root -l -b -q "run_trilepton_mumumu.cc(3)"
+root -l -b -q "run_trilepton_mumumu.cc(4)"
+root -l -b -q "run_trilepton_mumumu.cc(5)"
+root -l -b -q "run_trilepton_mumumu.cc(6)"
+root -l -b -q "run_trilepton_mumumu.cc(7)"
+root -l -b -q "run_trilepton_mumumu.cc(8)"
+
---
+++ script_run_trilepton_mumumu_CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root -l -b -q "run_trilepton_mumumu_CR.cc(0)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(1)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(2)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(3)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(4)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(5)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(6)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(7)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(8)"
+
+
