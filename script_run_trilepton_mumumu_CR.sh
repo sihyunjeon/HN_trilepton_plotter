0f96507ca6dd34707a3a6ba9c70ad9b6d20496a9
Jae Sung Kim <jae.sung.kim@cern.ch>
2016-08-30 18:07:02 +0900

    fix CR script

--- script_run_trilepton_mumumu_CR.sh
+++ script_run_trilepton_mumumu_CR.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-for c in "CR2"
-do
-  root -l -b -q "run_trilepton_mumumu_CR.cc(0,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(1,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(2,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(3,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(4,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(5,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(6,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(7,\"$c\")"
-  root -l -b -q "run_trilepton_mumumu_CR.cc(8,\"$c\")"
-done
-
-
+root -l -b -q "run_trilepton_mumumu_CR.cc(0)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(1)"
+root -l -b -q "run_trilepton_mumumu_CR.cc(2)"
